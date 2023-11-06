a6b3fdbf650a4134537f02264e758d0a0d510e7d
Kio <kio.thedev@gmail.com>
2023-11-06 08:47:32 -0500

    Reword SplitImageKC
    -Fix Typo
    -Add linkage to proper upstream (Sharkey is our parent, not Misskey)
    - Rewrite sentencing to be more community-centric
    - nya

--- components/landing/SplitImageKitsuClub.vue
+++ components/landing/SplitImageKitsuClub.vue
@@ -10,28 +10,28 @@
 						Fully featured microblogging
 					</h2>
 					<p class="text-base text-gray-200 md:text-lg">
-						KitsuClub and KitsuGay feature complete microblogging
+						KitsuClub and KitsuGay are feature complete microblogging
 						services with emojis, reacts, quote posts, animated
-						avatars, and more! We're always adding new features, so
-						you'll never get bored!
+						avatars, and more! With a vibrant community and new features being constantly added,
+						You'll never get bored!
 					</p>
 				</div>
 				<div class="grid gap-5 row-gap-8 sm:grid-cols-2">
-					<div
+					<a href="https://joinsharkey.org/
 						class="bg-dark-300 shadow-sm hover:ring-2 duration-200 overflow-hidden ring-purple-500 rounded">
 						<div
 							class="h-full p-5 rounded-r flex flex-col justify-between">
 							<h6
 								class="mb-2 font-semibold leading-5 text-gray-50">
-								Based on Misskey
+								Based on Sharkey
 							</h6>
 							<p class="text-sm text-gray-200">
-								KitsuClub and KitsuGay are based on Misskey, a
-								secure, open-source microblogging platform
+								KitsuClub and KitsuGay are based on Sharkey, a
+								secure, feature-rich, and open-source microblogging platform
 								written in Node.js.
 							</p>
 						</div>
-					</div>
+					</a>
 					<div
 						class="bg-dark-300 shadow-sm hover:ring-2 duration-200 overflow-hidden ring-purple-500 rounded">
 						<div
@@ -54,7 +54,7 @@
 					class="w-full h-56 rounded shadow-lg sm:h-96"
 					format="webp"
 					sizes="sm:100vw md:100vw lg:50vw"
-					src="/images/assets/screenshots/screenshot3.png"
+					src="https://s3.kitsunes.club/storage/c0953390-55b9-4474-b253-c3afff574a96.webp"
 					alt="" />
 			</div>
 		</div>
