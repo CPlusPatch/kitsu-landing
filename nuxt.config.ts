63315a71b4052fdbe3d6fe06fdd04c56f1f18058
Kio <kio.thedev@gmail.com>
2023-12-22 22:47:03 -0500

    remove sins

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-	plugins: [
-		{ src: '~/plugins/matamo.js', ssr: false}
-	],
 	modules: [
 		"@unocss/nuxt",
 		"nuxt-headlessui",
