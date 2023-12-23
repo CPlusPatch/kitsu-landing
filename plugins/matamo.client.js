db6f091b4125cd832639261328b990cb0e476ddf
Kio <kio.thedev@gmail.com>
2023-12-22 23:07:56 -0500

    matamo i just I JUST WANT YOU TO WORK

--- plugins/matamo.client.js
+++ plugins/matamo.client.js
@@ -3,7 +3,6 @@ import VueMatomo from 'vue-matomo'
 
 export default defineNuxtPlugin(nuxtapp => {
   nuxtapp.vueApp.use(VueMatomo, {
-    router: app.router,
     host: "https://analytics.kitsu.life",
     siteId: 2,
     router: nuxtapp.$router,
